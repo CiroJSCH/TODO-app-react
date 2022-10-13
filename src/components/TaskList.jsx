bc10d05c88864876ca1c34a23e52e6de3a6664ed
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2022-10-13 19:08:10 -0300

    Agregado renderizado condicional

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -12,6 +12,9 @@ const TaskList = () => {
     >
       <div className="form-floating col-lg-6 col-xs-12 mb-3 px-2">
         {
+          tasks.length === 0 && <h1>You don't have any tasks yet. Add one</h1>
+        }
+        {
           tasks.map((task, index) => {
             return <TaskItem key={index} id={index} task={task}/>
           })
