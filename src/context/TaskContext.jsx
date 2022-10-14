34a170b7e64f873743db4fb1b32a5c7fe8e23188
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2022-10-14 12:01:47 -0300

    Guardar tareas en local storage

--- src/context/TaskContext.jsx
+++ src/context/TaskContext.jsx
@@ -1,17 +1,30 @@
-import React,{ useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export const TaskContext = React.createContext();
 
 const TaskContextProvider = (props) => {
-
   const [tasks, setTasks] = useState([]);
 
   const createTask = (name, description) => {
-    setTasks([...tasks, {name, description, id: tasks.length}]);
-  }
+    setTasks([...tasks, { name, description, id: tasks.length }]);
+    localStorage.setItem("tasks", JSON.stringify([...tasks, { name, description, id: tasks.length }]));
+  };
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+    localStorage.setItem("tasks", JSON.stringify(tasks.filter((task) => task.id !== id)));
+  };
+
+  useEffect(() => {
+    const tasksInLocal = localStorage.getItem("tasks");
+    if (tasksInLocal !== null) {
+      const tasksArray = JSON.parse(tasksInLocal);
+      setTasks(tasksArray);
+    }
+  }, [tasks]);
 
   return (
-    <TaskContext.Provider value={{ tasks, createTask }}>
+    <TaskContext.Provider value={{ tasks, createTask, deleteTask }}>
       {props.children}
     </TaskContext.Provider>
   );
