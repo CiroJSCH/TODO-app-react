f9c788a8bd44cc344b4008924ba997afe56bc8de
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2022-10-15 14:38:26 -0300

    Estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import TaskForm from "./components/TaskForm";
 import TaskList from "./components/TaskList";
+import Footer from "./components/Footer";
 import "./styles/App.css";
 
 function App() {
 
   return (
-    <div className="container text-center mt-3 d-flex flex-column justify-content-center">
-      <h1 style={{color: "darkblue"}}>Todo List</h1>
-      <TaskForm />
-      <TaskList />
-    </div>
+    <>
+      <div className="container text-center mt-3 d-flex flex-column min-vh-100">
+        <h1 style={{color: "darkblue"}}>Todo List</h1>
+        <TaskForm />
+        <TaskList />
+      </div>
+      <Footer />
+    </>
   );
 }
 
