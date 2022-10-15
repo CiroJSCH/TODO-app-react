3d605671c618f7ae6de79ec6fb2ef46fd2ada6f4
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2022-10-14 21:28:34 -0300

    Switch para completar tareas

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import DeleteIcon from "@mui/icons-material/Delete";
+import { TaskContext } from "../context/TaskContext";
+import Switch from '@mui/material/Switch';
+import Complete from "./Complete";
 
 const TaskItem = ({ task, id }) => {
+
+  const { deleteTask, toggleComplete } = useContext(TaskContext);
+
   return (
     <div className="accordion-item">
-      <h2 className="accordion-header" id={`heading${id}`}>
+      <h2 className={`accordion-header ${task.completed ? "opacity-50" : null}`} id={`heading${id}`}>
         <button
           className="accordion-button collapsed"
           type="button"
@@ -22,14 +28,16 @@ const TaskItem = ({ task, id }) => {
         aria-labelledby={`heading${id}`}
         data-bs-parent="#accordionExample"
       >
-        <div className="accordion-body">
+        <div className={`accordion-body ${task.completed ? "opacity-50" : null}`}>
           {task.description}
         </div>
-        <DeleteIcon style={{position: "relative", left: "47%", bottom: "5px", color: "red"}}/>
+        <div className="d-flex align-items-center justify-content-between border-top ps-2">
+          <Complete toggleComplete={toggleComplete} task={task}/>
+          <DeleteIcon className="mx-1" style={{color: "darkred", cursor: "pointer"}} onClick={() => deleteTask(task.id)}/>
+        </div>
       </div>
     </div>
   );
 };
 
 export default TaskItem;
-
